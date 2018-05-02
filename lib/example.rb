01f4b50e0f8eecbe569b46066750a2ec2a1931e6
Rebecca Tong <rtong@buffalo.edu>
2018-05-02 18:33:20 +0000

    automatically backed up by learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,14 +1,8 @@
 class Person
   attr_accessor :name, :age, :gender, :location, :birth_city, :nationality, :favorite_ice_cream
 
-  def initialize(name, age, gender, location, birth_city, nationality, favorite_ice_cream)
-    @name = name
-    @age = age
-    @gender = gender
-    @location = location
-    @birth_city = birth_city
-    @nationality = nationality
-    @favorite_ice_cream = favorite_ice_cream
+  def initialize(attr_hash={})
+    attr_hash.each {|attr_name, attr_value| self.send("#{attr_name}=", attr_value)}
   end
 
   def talk(what_to_say="hello")
