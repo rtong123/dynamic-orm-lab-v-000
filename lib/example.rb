34624b654fc684ced287870238157337de25cbe7
Rebecca Tong <rtong@buffalo.edu>
2018-05-02 18:29:13 +0000

    automatically backed up by learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,4 +1,17 @@
 class Person
+class Person
+  attr_accessor :name, :age, :gender, :location, :birth_city, :nationality, :favorite_ice_cream
+
+  def initialize(name, age, gender, location, birth_city, nationality, favorite_ice_cream)
+    @name = name
+    @age = age
+    @gender = gender
+    @location = location
+    @birth_city = birth_city
+    @nationality = nationality
+    @favorite_ice_cream = favorite_ice_cream
+  end
+
   def talk(what_to_say="hello")
     puts what_to_say
   end
